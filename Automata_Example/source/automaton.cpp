0998ebf55f3dbdbd169135b7253d1cda302b6cd4
Anastasia Anichenko <anastasia@eduroam-int-dhcp-97-42-120.ucl.ac.uk>
2024-01-26 16:05:57 +0000

    Updated automaton to make it a bit more robust

--- Automata_Example/source/automaton.cpp
+++ Automata_Example/source/automaton.cpp
@@ -1,4 +1,6 @@
 #include "automaton.h"
+#include <stdexcept>
+#include <iostream>
 
 Automaton::Automaton(map<char, int> A, vector<vector<int>> M, vector<int> S) : alphabet(A), transition_matrix(M), accepting_states(S) {}
 
@@ -12,6 +14,10 @@ bool Automaton::Read(string word)
         // -> can be used to access methods of the key value pair
         auto it = alphabet.find(c);
 
+        if(it == alphabet.end()) {
+            throw std::logic_error("Invalid operation alphabet does not contain characters ");
+        }
+ 
         //it->first gives the key, it->second gives the value
         int j = it->second;
         state = transition_matrix[state][j];
--- Automata_Example/source/main.cpp
+++ Automata_Example/source/main.cpp
@@ -13,8 +13,13 @@ int main()
     map<char, int> A{{'a', 0}, {'b', 1}}; 
     vector<int> S{1};
     Automaton ends_with_b(A, M, S);
-    std::cout << "Read abab = " << ends_with_b.Read("abab") << std::endl;
-    std::cout << "Read aaba = " << ends_with_b.Read("aaba") << std::endl;
+    try {
+        std::cout << "Read abab = " << ends_with_b.Read("abab") << std::endl;
+        std::cout << "Read aaba = " << ends_with_b.Read("aaba") << std::endl;
+        std::cout << "Read zaaba = " << ends_with_b.Read("zaaba") << std::endl;
+    } catch (std::logic_error &e) {
+        std::cerr << e.what() << std::endl;
+    }
 
     return 0;
 }
